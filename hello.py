278965a4b46d6e67aefa06c6906a350602b23e96
Osman <ozuberi@umich.edu>
2018-11-29 19:27:40 -0500

    saved hello

--- hello.py
+++ hello.py
@@ -59,13 +59,13 @@ def conversation(title, body, session_attributes):
     speechlet['shouldEndSession'] = False
     return build_response(speechlet, session_attributes=session_attributes)
 
-
-def statement(title, body):
-    speechlet = {}
-    speechlet['outputSpeech'] = build_PlainSpeech(body)
-    speechlet['card'] = build_SimpleCard(title, body)
-    speechlet['shouldEndSession'] = True
-    return build_response(speechlet)
+#
+# def statement(title, body):
+#     speechlet = {}
+#     speechlet['outputSpeech'] = build_PlainSpeech(body)
+#     speechlet['card'] = build_SimpleCard(title, body)
+#     speechlet['shouldEndSession'] = True
+#     return build_response(speechlet)
 
 
 def continue_dialog():
